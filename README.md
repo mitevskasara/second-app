b8631575063590efe6df75fa0cf7563755302971
Sara Mitevska <sara.mitevska@enterpriseleague.com>
2023-05-16 21:39:23 +0200

    setup project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # second-app
+
+This repository is part of an Example of code-sharing between different Git repositories.
+It uses a shared Button component from https://github.com/mitevskasara/library
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Button from './components/button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Example of code-sharing between different Git repositories
+        </p>
+        <Button />
+      </header>
+    </div>
+  );
+}
+
+export default App;
